c6621d25dcfa243f821dc4ac98fb3a15d47dec41
fanglm <ooohmydawn@hotmail.com>
2023-04-08 15:27:54 +0800

    fix: mjs file error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { program } from 'commander'
+import { Command } from 'commander'
 import { version } from '../package.json'
-
+const program = new Command()
 program.version(version)
 program.parse(process.argv)
